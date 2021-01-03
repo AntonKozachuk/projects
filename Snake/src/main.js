c5fd89b20f315033d04c17e772d3f6b902054097
Anton Kozachuk <54616703+AntonKozachuk@users.noreply.github.com>
2021-01-03 21:51:16 +0200

    Add funcs to create board and start a game

--- Snake/src/main.js
+++ Snake/src/main.js
@@ -15,4 +15,43 @@ let grid = document.querySelector('.grid'),
     intervalTime = 0,
     interval = 0;
 
+document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener('keyup', control);
+    createBoard();
+    startGame();
+    playAgain.addEventListener('click', replay);
+});
+
+
+function createBoard() {
+    popup.style.display = 'none';
+    for(let i = 0; i < 100; i++) {
+        let div = document.createElement('div');
+        grid.appendChild(div);
+    }
+};
+
+function startGame() {
+    let squares = document.querySelectorAll('.grid div');
+    randomApple(squares);
+    //random apple
+    direction = 1;
+    scoreDisplay.innerHTML = score;
+    intervalTime = 1000;
+    currentSnake = [2, 1, 0];
+    currentIndex = 0;
+    currentSnake.forEach(index => squares[index].classList.add('snake'));
+    interval = setInterval(moveOutcome, intervalTime);
+};
+
+function moveOutcome() {
+    let squares = document.querySelectorAll('.grid div');
+    if(checkForHits(squares)) {
+        alert('You hit something!');
+        popup.style.display = 'flex';
+        return clearInterval(interval);
+    } else {
+        moveSnake(squares);
+    }
+};
 
